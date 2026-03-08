aafb1612f286e30469c80c410463bc6c2efb6493
zing <zing@gmail.com>
2026-03-08 14:29:17 +0800

    新增動態區間提示

--- game.py
+++ game.py
@@ -10,23 +10,26 @@ import random
 start,end=1,100
 count=10
 
-# 產生亂數
 x=random.randint(start,end)    
-# 提示答案
-#print(x)
 
 for i in range(count):
     y=int(input(f"猜第{i+1}/{count}次,請猜一個數字({start}~{end}):"))
     
-    # 猜對 + break
+    if y<start or y>end:
+        print("不要亂")
+        continue
     if x==y:
         print("恭喜猜對!")
         break
-    
-    # 猜錯
+
     if x>y:
+        if y>start:
+            start=y+1
         print("猜大一點")
     else:
+        if y<end:
+            end=y-1
+
         print("猜小一點")
     
 
